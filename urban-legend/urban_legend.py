967fc542033a693fc062223dc94cd32e053ad905
Arafat Eajaz <arafat.eajaz@gmail.com>
2016-10-14 17:58:50 +0530

    .

--- urban-legend/urban_legend.py
+++ urban-legend/urban_legend.py
@@ -9,12 +9,27 @@ from utils import utils
 _INPUT_TEST_DATA = 'urban-legend/testdata/input.txt'
 
 
-def urban_legend():
-  for search_engines, queries in utils.GetInputData(_INPUT_TEST_DATA, 2):
+def calculate_number_of_switch(search_engines, queries):
+  """Calculates the number of switches.
 
+  Considering the fact that implementation is optimized for number of switches.
+  Also first choice of search engine will not be considered as a switch.
+  Queries are processed in the order they appear.
+
+  Args:
+    search_engines: list of search engines.
+    queries: list of queries to process.
+
+  Returns: int, number of switches required.
+  """
+  engine = search_engines.copy
+  switch = 0
+  for q in queries:
     pass
 
 
 if __name__ == '__main__':
-  urban_legend()
+  for search_engines, queries in utils.GetInputData(_INPUT_TEST_DATA, 2):
+    number_of_switch = calculate_number_of_switch(search_engines, queries)
+    print 'Number of switches required %s' %number_of_switch
   print 'Done'

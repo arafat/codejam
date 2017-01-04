22ccee07fd4639373e095b4e5a0979539a75649f
Arafat Eajaz <arafat@arafat-linux.gur.corp.google.com>
2017-01-04 17:48:27 +0530

    .

--- minimum_scalar_product/minimum_scalar_product.py
+++ minimum_scalar_product/minimum_scalar_product.py
@@ -1,10 +1,16 @@
+from timeit import timeit
 from utils import utils
 
 _INPUT_DATA_FILE_LOCATION = 'minimum_scalar_product/test_data/minimum_scalar_product.in'
 
 def CalculateMinimumScalarProduct(scalar1, scalar2):
-  input = utils.GetInputData(_INPUT_DATA_FILE_LOCATION, 1, 2)
-  import pdb; pdb.set_trace()
   scalar1 = sorted(scalar1)
   scalar2 = sorted(scalar2, reverse=True)
-  return sum([i*j for i, j in zip(scalar1, scalar2)])
+  print sum([i*j for i, j in zip(scalar1, scalar2)])
+
+def Run():
+  in_iter = utils.GetInputData(_INPUT_DATA_FILE_LOCATION, 1, 2)
+  for input in in_iter:
+    scalar1 = [int(i) for i in input[0][0].split(' ')]
+    scalar2 = [int(i) for i in input[0][1].split(' ')]
+    CalculateMinimumScalarProduct(scalar1, scalar2)

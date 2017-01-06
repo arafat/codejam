d65260cd2ddbe9052aa5b311416d15607a554475
Arafat Eajaz <arafat@arafat-linux.gur.corp.google.com>
2017-01-06 15:24:37 +0530

    .

---
+++ always_turn_left/always_turn_left.py
@@ -0,0 +1,15 @@
+"""Programs to implement Always Turn Left problem from Google Code Jam.
+
+Problem Url: https://code.google.com/codejam/contest/32003/dashboard#s=p1
+"""
+
+from utils import utils
+import config
+
+_INPUT_FILE_PATH = 'always_turn_left/test_data/always_turn_left.in'
+
+
+def Run():
+  in_iter = utils.GetInputData(_INPUT_FILE_PATH, 1, 1)
+  import pdb; pdb.set_trace()
+  print 'This is Run method.'
